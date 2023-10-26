497e62d57b19d10dc531f3483a79bf6017116502
JeongukChoi <onsucjuk@gmail.com>
2023-10-27 08:11:06 +0900

    테스트 컨트롤러

--- src/main/java/dp/fdis/controller/testController.java
+++ src/main/java/dp/fdis/controller/testController.java
@@ -1,2 +1,24 @@
-package dp.fdis.controller;public class testController {
+package dp.fdis.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequestMapping(value = "/test")
+@RequiredArgsConstructor
+@Controller
+public class testController {
+
+    @GetMapping(value = "test")
+    public String myAddrReg() {
+
+        log.info(this.getClass().getName() + ".test Start!");
+
+        log.info(this.getClass().getName() + ".test End!");
+
+        return "thymeleaf/notice/tables";
+    }
 }
